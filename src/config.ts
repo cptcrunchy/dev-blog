ea754cfc417788795b959355f00496b3f94a5670
Cpt_Crunch <jasong84@gmail.com>
2024-05-07 20:23:44 -0500

    Chore: Cleaned up some components

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,8 @@ import type {
   NavBarConfig,
   ProfileConfig,
   SiteConfig,
-} from './types/config'
-import { LinkPreset } from './types/config'
+} from '@/types/config'
+import { LinkPreset } from '@/types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'Jason Gutierrez',
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,7 @@
 import { defineCollection, z } from 'astro:content'
 
 const postsCollection = defineCollection({
+  type: 'content',
   schema: z.object({
     title: z.string(),
     published: z.date(),
